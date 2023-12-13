958b453a5d7b33d18e5964275e824acfc4a8e12e
themer <themer.saanouni@agence-inspire.com>
2023-12-13 18:04:48 +0100

    finishing the app

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 
-import styles from './company.style'
+import { icons } from '../../../constants';
+import { checkImageURL } from '../../../utils';
 
-const Company = () => {
+import styles from './company.style';
+
+const Company = ({ CompanyLogo, jobTitle, CompanyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image source={{ uri: CompanyLogo }} style={styles.logoImage} />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{CompanyName}</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,15 +1,47 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = () => {
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  return (
-    <View>
-      <Text>useFetch</Text>
-    </View>
-  );
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': '9d60f69fd4msh2f9c17fc780ec6ap1f0878jsn3289c1ae82fe',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+    params: {
+      ...query,
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
 
 export default useFetch;
